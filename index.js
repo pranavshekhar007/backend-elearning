8917e404d5e67a6581f086239263916b2200f323
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-04-22 14:38:51 +0530

    add cors in index

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const routes = require("./src/route");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
 
 const app = express();
-const server = createServer(app);
 
 const allowedOrigins = [
-  "https://frontend-elearning-beta.vercel.app", // Ensure no trailing slash
-  "http://localhost:3000", // For local development
+  "https://frontend-elearning-beta.vercel.app",
+  "http://localhost:3000",
 ];
 
 // CORS configuration
@@ -19,7 +16,7 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow requests from listed origins or localhost
+        callback(null, true);
       } else {
         callback(new Error("CORS policy does not allow this origin"));
       }
@@ -32,18 +29,10 @@ app.use(
 
 // Apply fallback CORS headers manually (as a fallback)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // You can specify the origin if needed
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
-});
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
 });
 
 // Static file handling for uploads
@@ -64,12 +53,6 @@ mongoose
     console.error("DB connection error:", err);
   });
 
-// Pass the `io` instance to routes
-app.use((req, res, next) => {
-  req.io = io; // Attach the `io` object to the request
-  next();
-});
-
 // Routes
 app.get("/", (req, res) => res.send(`Server listing on port ${PORT}`));
 app.use("/api", routes);
@@ -77,6 +60,6 @@ app.all("*", (req, res) => res.status(404).json({ error: "404 Not Found" }));
 
 // Start the server
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () =>
+app.listen(PORT, () =>
   console.log(`Server running on ${PORT}`)
 );
