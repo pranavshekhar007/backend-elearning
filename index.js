ba6d1113214c6cb0b06c9ba7d63a37cbfbc8733a
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-04-22 15:30:24 +0530

    add cors in index

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const path = require("path");
 const routes = require("./src/route");
+const {createServer} = require('http');
+const {Server} = require("socket.io")
 
 const app = express();
+const server = createServer(app);
 
-// Middleware
-app.use(cors({
-  origin: [
-    "https://frontend-elearning-beta.vercel.app",
-    "http://localhost:3000"
-  ],
-  credentials: true,
-}));
+const io = new Server(server, {
+  cors:{
+    origin:"*",
+    methods:["GET", "POST"],
+    credentials:true,
+  }
+})
+
+app.use('/uploads', express.static('uploads'))
+const PORT = process.env.PORT || 8000;
+
+
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Static files
-app.use('/uploads', express.static('uploads'));
 
-// DB Connection
-mongoose.connect(process.env.DB_STRING)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ DB Error:", err));
 
-// Routes
-app.get("/", (req, res) => res.send("Backend is working"));
+
+// connecting with database
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_STRING
+).then(()=>{
+    console.warn("db connection done again")
+})
+
+// Pass the `io` instance to routes
+app.use((req, res, next) => {
+  req.io = io; // Attach the `io` object to the request
+  next();
+});
+
+app.get("/", (req, res) => res.send(`Server listing on port ${PORT}`));
 app.use("/api", routes);
-app.all("*", (req, res) => res.status(404).json({ error: "404 Not Found" }));
+app.all("*", (req, res) => res.status(404).json({ error: "404 Not Found" })); 
 
-// ✅ Export app for Vercel
-module.exports = app;
+
+
+server.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
+);
